53b889ebc7ceb8d29f1498e3abcd1ddae0fe638c
ismatullayevs <ismatullayevpro@gmail.com>
2022-07-13 18:05:16 +0500

    Seperate settings

--- ustadavron/wsgi.py
+++ ustadavron/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ustadavron.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ustadavron.settings.production')
 
 application = get_wsgi_application()
