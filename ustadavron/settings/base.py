5978eef409fce7cf6e5f8c25719161e1679b45ed
ismatullayevs <ismatullayevpro@gmail.com>
2022-07-14 21:36:09 +0500

    Configured AWS

--- ustadavron/settings/base.py
+++ ustadavron/settings/base.py
@@ -126,4 +126,3 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
